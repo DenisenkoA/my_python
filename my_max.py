5995df4df51be13f59ad6dfcbe770994970b0a0f
DenisenkoA <49762996+DenisenkoA@users.noreply.github.com>
2019-04-18 21:52:13 +0300

    Add files via upload

---
+++ my_max.py
@@ -0,0 +1,11 @@
+x=input("Введите первое число\n")
+y=input("Введите второе число\n")
+
+if x:
+    if x==y:
+        print("Оба числа равны",x,"=",y)
+    elif x<=y:
+        print (y)
+    else:
+        print (x)
+        
